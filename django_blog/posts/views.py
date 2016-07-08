186df6189ced28c2d641d6648acfe1573395c421
Zachary King <kingzach77@gmail.com>
2016-07-08 08:03:08 -0500

    Created other basic views for posts

--- django_blog/posts/views.py
+++ django_blog/posts/views.py
@@ -4,5 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def posts_home(request):
-    return HttpResponse("<h1>Hello</h1>")
+def posts_create(request):
+    return HttpResponse("<h1>Create</h1>")
+
+def posts_detail(request):
+    return HttpResponse("<h1>Detail</h1>")
+
+def posts_list(request):
+    return HttpResponse("<h1>List</h1>")
+
+def posts_update(request):
+    return HttpResponse("<h1>Update</h1>")
+
+def posts_delete(request):
+    return HttpResponse("<h1>Delete</h1>")
