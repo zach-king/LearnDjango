7db53f1f2e3a03329b37509ef43087db0494e638
Zachary King <kingzach77@gmail.com>
2016-07-08 15:51:37 -0500

    Create Form added

--- django_blog/posts/views.py
+++ django_blog/posts/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-# Create your views here.
+from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        print(form.cleaned_data.get('title'))
+        instance.save()
+
+    context = {
+        'form': form,
+        'title': 'Create Post'
+    }
+    return render(request, 'post_form.html', context)
 
 def post_detail(request, id=None):
     instance = get_object_or_404(Post, id=id)
